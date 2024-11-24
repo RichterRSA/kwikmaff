9a449faefa18356245688a50ad47ddd2cf48bba4
u04576617 <u04576617@tuks.co.za>
2024-11-24 11:24:26 +0200

    Updates

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const heading = document.querySelector("h1");
+    if (heading) {
+        heading.textContent = "Hello from TypeScript!";
+    }
+});
